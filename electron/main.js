40dcef8bdba48f36289db37a3df93236484966bd
SnapTST <cursoruser65@gmail.com>
2025-08-03 21:04:26 +0530

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // This loads TeraBox web login
+  win.loadURL("https://www.terabox.com/");
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
 // Electron main process
